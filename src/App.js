916427255606063826a3d51b878e5f8f24a093ab
mateuspadilha166 <mateuspadilha873@gmail.com>
2024-08-26 20:02:29 -0300

    Funcao de clicar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks: 0
+    };
+  }
+
+  handleDocumentClick = () => {
+    this.setState((prevState) => ({
+      clicks: prevState.clicks + 1
+    }));
+  };
+
+  componentDidMount() {
+    document.addEventListener("click", this.handleDocumentClick);
+  }
+
+  componentDidUpdate() {
+    console.log(`Atualizou: ${this.state.clicks} cliques`);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("click", this.handleDocumentClick);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Quantidade de cliques: {this.state.clicks}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
